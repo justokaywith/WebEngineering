057e797c08b60eb85d1ad0a1c5b8e97cceceaefe
Haris <harisrana99@gmail.com>
2020-11-20 18:32:48 +0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
 <header>
-	<span><a href="">Home</a></span>
-		<span><a href="">Appointments</a></span>
-			<span><a href="">About</a></span>
+	<span><a href="../index.php">Home</a></span>
+		<span><a href="subpages/appointments.php">Appointments</a></span>
+			<span><a href="about.php">About</a></span>
+				<span><a href="login.php">Login</a></span>
 
 </header>
