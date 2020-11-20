34327f4a1f7d0c619ae9d392f83d8168849bc894
Haris <harisrana99@gmail.com>
2020-11-20 19:38:58 +0500

    changes done in header php and style css

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 
 <header>
-	<a href="index.php">Index</a>
-	<a href="./subpages/appointments.php">Appointments</a>
-	<a href="./subpages/about.php">About</a>
-	<a href="./subpages/login.php">Login</a>
+	<span class="sec_header_1"><a href="index.php">Index</a></span>
+	<span class="sec_header_2"><a href="./subpages/appointments.php">Appointments</a></span>
+	<span class="sec_header_3"><a href="./subpages/about.php">About</a></span>
+	<span class="sec_header_4"><a href="./subpages/login.php">Login</a></span>
 </header>
