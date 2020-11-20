b963076706392f265025aef1a72bb5772b3bcbe9
Haris <harisrana99@gmail.com>
2020-11-20 19:03:09 +0500

    updates

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer>
-	&copy; Copyrights Claimed, All rights reserved, <span class="sec_footer"> Riphah International University Islamabad</span> 2018 - <?php echo date("Y"); ?> <span class="sec_footer_2"> <br>Credit goes to Sir Saud Khan</span>
+	<p>&copy; Copyrights Claimed, All rights reserved. <span class="sec_footer"> Riphah International University Islamabad. </span> 2018 - <?php echo date("Y"); ?><br>Credit goes to  <span class="sec_footer_2">Sir Saud Khan</span></p>
 </footer>
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,5 @@
+
 <header>
 	<a href="index.php">Index</a>
+	<a href="/subpages/appointments.php"></a>
 </header>
