94dc3cee7a6b4279e37a4029ba573b85822fa17d
Haris <harisrana99@gmail.com>
2020-11-21 08:40:04 +0500

    updated

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,6 @@
 
 <header>
 		<a href="index.php">Home</a>
-		<a href="./subpages/appointments.php">Appointments</a>
 		<a href="./subpages/about.php">About</a>
 		<a href="./subpages/signup.php">Signup</a>
 </header>
