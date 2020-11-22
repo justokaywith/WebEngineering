ff58121b43d5bcfb0181d537524d86fc54ba78ff
Haris <harisrana99@gmail.com>
2020-11-22 21:40:32 +0500

    updated
    made it iphone 12 and 720 p responsive

--- subpages/main.php
+++ subpages/main.php
@@ -1,57 +1,57 @@
 <div class="main">
   <h1>The <span class="foodies_logo">Foodies</span></h1>
  	<div class="specials">
- 		<p>Specials</p>
+ 		<label>Specials</label>
  	</div>
 
- <! ------------------Special Item 1 starts here >
+ <!------------------Special Item 1 starts here----->
 
-  <div class="sec_1_main">
+  <div class="spec">
   	<label>The King Hamburger</label>
   	<img src="./graphics/burger.webp">
-  	<p>Try out our best hamburger. <br><br><span class="only_for_1">Only for</span><br><span class="price_1"> Rs 299.00</span>
+  	<p>Try out our best hamburger. <br><br><span class="only_for">Only for</span><br><span class="price"> Rs 299.00</span>
   	</p>
   	<a href="#">Order Here</a>
   	<hr>
   </div>
 
-  <! ------------------Special Item 2 starts here >
+  <!------------------Special Item 2 starts here----->
 
-    <div class="sec_2_main">
+    <div class="spec">
   	<label>Chick n'Nuggets</label>
  	
   	<img src="./graphics/nuggets.webp">
-  	<p>Best in town Chicken & Nuggets.<br><br><span class="only_for_2">Only for</span><br><span class="price_2"> Rs 269.99</span>
+  	<p>Best in town Chicken & Nuggets.<br><br><span class="only_for">Only for</span><br><span class="price"> Rs 269.99</span>
   	</p>
   	<a href="#">Order Here</a>
   	<hr>
   </div>
 
-   <! ------------------Special Item 3 starts here >
+   <!-------------------Special Item 3 starts here--->
 
-    <div class="sec_3_main">
+    <div class="spec">
   	<label>E che pizza!</label>
  	
   	<img src="./graphics/pizza.webp">
-  	<p>We'll serve it hot. Get the best pizza in town. <br><br><span class="only_for_3">Starts From</span><br><span class="price_3"> Rs 800.00 <span class="inches">for 8"</span></span>
+  	<p>We'll serve it hot. Get the best pizza in town. <br><br><span class="only_for">Starts From</span><br><span class="price"> Rs 800.00 <span class="inches">for 8"</span></span>
   </p>
   	<a href="#">Order Here</a>
   	<hr>
   </div>
 
-     <! ------------------Special Item 4 starts here >
+     <!------------------Special Item 4 starts here---->
 
-    <div class="sec_4_main">
+    <div class="spec">
   	<label>The Karma Shuarma</label>
  	
   	<img src="./graphics/shuarma.webp">
-  	<p>Finger lickin' good.The best of the best.The karma shuarma! <br><br><span class="only_for_4">Only From</span><br><span class="price_4"> Rs 250.00</span>
+  	<p>Finger lickin' good.The best of the best.The karma shuarma! <br><br><span class="only_for">Only From</span><br><span class="price"> Rs 250.00</span>
   </p>
   	<a href="#">Order Here</a>
   	<hr>
   </div>
 
-   <! ------------------Deals starts here >
+   <!------------------Deals starts here----->
    <div class="deals">
  		<label>Deals</label>
  		<p>So you're one of those guys ? <br>
@@ -59,9 +59,9 @@
  			<span>We Got the best deals in town for you.</span></p>
  	</div>
 
- 	 <! ------------------Deal 1 starts here >
+ 	 <!------------------Deal 1 starts here------->
 
- 	    <div class="deal_1">
+ 	    <div class="deal">
   	<label>Deal 1</label>
  	
   	<img src="./graphics/deal1.webp">
@@ -72,9 +72,9 @@
   	<hr>
   </div>
 
-   <! ------------------Deal 2 starts here >
+   <!------------------Deal 2 starts here-------->
 
- 	    <div class="deal_1">
+ 	    <div class="deal">
   	<label>Deal 2</label>
  	
   	<img src="./graphics/deal2.webp">
@@ -83,6 +83,13 @@
   </p>
   	<a href="#">Order Here</a>
   	<hr>
-  </div
+  </div>
+
+	<!-------------- end of deals-------->
+
+	<div class="end_of_deals">
+		<p>Find out more <span class="end_span">Interesting</span> deals <br><span class="end_span">Order </span>them <a href="#">Here</a></p>
+	</div>
+
 
 </div>
