48e3e25764e56a162ad3447427962781a0cfaf24
Haris Rana <harisrana99@gmail.com>
2020-11-24 22:10:13 +0500

    database

--- subpages/main.php
+++ subpages/main.php
@@ -120,7 +120,7 @@
 
 <form action="./includes/insert.php" method="POST">
     <div class="end_of_deals">
-     <p>Get updates on all special menu items and deals. Subscribe here</p><input type="email" name="email" value="email" placeholder="Email Address" required>
+     <p>Get updates on all special menu items and deals. Subscribe here</p><input type="email" name="email" value="" placeholder="Email Address" required>
      <button type="submit" value="" name="button">Submit</button>
   </div>
 </form>
