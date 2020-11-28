8dd8c748b7f57bdb5f360e474965353c5ff50545
Haris Rana <harisrana99@gmail.com>
2020-11-29 02:58:33 +0500

    up

--- subpages/about.php
+++ subpages/about.php
@@ -1,12 +1,106 @@
 <!DOCTYPE html>
 <html>
-<head>
-	 <?php include '../includes/config.php'; ?>
+	<head>
+	
+	<?php include '../includes/config.php'; ?>
     <?php include $meta_subpages; ?>
     <link rel="stylesheet" href="<?php echo $css_subpages; ?>">
 	<title><?php echo $about; ?></title>
-</head>
-<body>
+	
+	</head>
 
-</body>
+	<body id="about_body">
+		
+	
+
+	<div id="dev_team">
+	
+		<?php include $url_header_subpages; ?>
+
+		<label>Introduction.</label>
+		<p>A Software Engineering team who thinks that with efforts they can be in the line of this tough developing competition.</p>
+
+		<label>Was it good?.</label>
+		<p>Simple yet time taking this project was one of the most fun projects we have done so far.</p>
+
+		<label>What we learned.</label>
+		<p>Team management is the most best thing in suck projects not only you provide yourself with ease but also it divides work load. </p>
+
+		<label>What we achieved.</label>
+		<p>Well if we see it this way, we have not achieved anything yet. but hey its always the fun and time we spent working together. you keep enjoing our products and we will keep making them</p>
+
+		<a href="#haris">Meet the devs</a>
+
+	</div>
+
+	<div id="haris">
+				
+		<label>Introduction.</label>
+		<p>A Software Engineering team who thinks that with efforts they can be in the line of this tough developing competition.</p>
+
+		<label>Was it good?.</label>
+		<p>Simple yet time taking this project was one of the most fun projects we have done so far.</p>
+
+		<label>What we learned.</label>
+		<p>Team management is the most best thing in suck projects not only you provide yourself with ease but also it divides work load. </p>
+
+		<label>What we achieved.</label>
+		<p>Well if we see it this way, we have not achieved anything yet. but hey its always the fun and time we spent working together. you keep enjoing our products and we will keep making them</p>
+
+		<a href="#osama">Osama Ali Mangi</a>
+	</div>
+
+	<div id="osama">
+				
+		<label>Introduction.</label>
+		<p>A Software Engineering team who thinks that with efforts they can be in the line of this tough developing competition.</p>
+
+		<label>Was it good?.</label>
+		<p>Simple yet time taking this project was one of the most fun projects we have done so far.</p>
+
+		<label>What we learned.</label>
+		<p>Team management is the most best thing in suck projects not only you provide yourself with ease but also it divides work load. </p>
+
+		<label>What we achieved.</label>
+		<p>Well if we see it this way, we have not achieved anything yet. but hey its always the fun and time we spent working together. you keep enjoing our products and we will keep making them</p>
+
+		<a href="#zain">Zain Ul Abidin</a>
+	</div>
+
+		<div id="zain">
+				
+		<label>Introduction.</label>
+		<p>A Software Engineering team who thinks that with efforts they can be in the line of this tough developing competition.</p>
+
+		<label>Was it good?.</label>
+		<p>Simple yet time taking this project was one of the most fun projects we have done so far.</p>
+
+		<label>What we learned.</label>
+		<p>Team management is the most best thing in suck projects not only you provide yourself with ease but also it divides work load. </p>
+
+		<label>What we achieved.</label>
+		<p>Well if we see it this way, we have not achieved anything yet. but hey its always the fun and time we spent working together. you keep enjoing our products and we will keep making them</p>
+
+		<a href="#abdullah">Abdullah</a>
+	</div>
+
+		<div id="abdullah">
+				
+		<label>Introduction.</label>
+		<p>A Software Engineering team who thinks that with efforts they can be in the line of this tough developing competition.</p>
+
+		<label>Was it good?.</label>
+		<p>Simple yet time taking this project was one of the most fun projects we have done so far.</p>
+
+		<label>What we learned.</label>
+		<p>Team management is the most best thing in suck projects not only you provide yourself with ease but also it divides work load. </p>
+
+		<label>What we achieved.</label>
+		<p>Well if we see it this way, we have not achieved anything yet. but hey its always the fun and time we spent working together. you keep enjoing our products and we will keep making them</p>
+
+		<a href="#dev_team">to top</a>
+	</div>
+
+	</body>
+
 </html>
